672950131aa344ac0783d66c23135de91b30c46d
Rashen97 <45933484+Rashen97@users.noreply.github.com>
2024-03-01 00:57:40 +0000

    changes
    all changes made

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-const express = require('express')
-const path = require('path')
-const stocks = require('./stocks')
+const express = require("express");
+const path = require("path");
+const stocks = require("./stocks");
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+const app = express();
+app.use(express.static(path.join(__dirname, "static")));
 
-app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
-})
+app.get("/stocks", async (req, res) => {
+  const stockSymbols = await stocks.getStocks();
+  res.send({ stockSymbols });
+});
 
-app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
-})
+app.get("/stocks/:symbol", async (req, res) => {
+  const {
+    params: { symbol },
+  } = req;
+  const data = await stocks.getStockPoints(symbol, new Date());
+  res.send(data);
+});
 
-app.listen(3000, () => console.log('Server is running!'))
+app.get("/stocks/:symbol", async (req, res) => {
+  const {
+    params: { symbol },
+  } = req;
+  try {
+    const data = await stocks.getStockPoints(symbol, new Date());
+
+    //stimulate a 10% failure rate
+    if (Math.random() < 0.1) {
+      throw new Error("Failure");
+    }
+    res.send(data);
+  } catch (error) {
+    res.status(500).send({ error: "Failure to retrieve stock data" });
+  }
+});
+
+app.listen(3000, () => console.log("Server is running!"));
