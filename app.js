3d093f50d394259d1d3dd39d95702ef50a8639d0
Rashen97 <45933484+Rashen97@users.noreply.github.com>
2024-03-01 10:48:29 +0000

    Challenge
    added necessary function

--- app.js
+++ app.js
@@ -18,4 +18,52 @@ app.get("/stocks/:symbol", async (req, res) => {
   res.send(data);
 });
 
+app.get("/stocks/:symbol", async (req, res) => {
+  const {
+    params: { symbol },
+  } = req;
+  try {
+    const data = await stocks.getStockPoints(symbol, new Date());
+    //10% failure
+    if (Math.random() < 0.1) {
+      throw new Error("failure");
+    }
+    res.send(data);
+  } catch (error) {
+    res.status(500).send({ error: "Failed to retrieve stock data" });
+  }
+});
+
+async function fetchStocks() {
+  try {
+    const response = await fetch("/stocks");
+    const { stockSymbols } = await response.json();
+    return stockSymbols;
+  } catch (error) {
+    console.error("Failed to fetch stocks", error);
+  }
+}
+
+async function fetchStockData(symbol) {
+  try {
+    const response = await fetch("/stocks/${symbol}");
+    const data = await response.json();
+    console.log("Data for ${symbol}:", data);
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch data for ${symbol} ", error);
+  }
+}
+
+async function loadData() {
+  const spinner = document.querySelector(".spinner");
+  const symbol = await fetchStocks();
+
+  const dataPromises = symbols.map((symbol) => fetchStockData(symbol));
+
+  Promise.all(dataPromises).then(() => {
+    spinner.style.display = "none";
+  });
+}
+
 app.listen(3000, () => console.log("Server is running!"));
--- static/scripts.js
+++ static/scripts.js
@@ -17,6 +17,40 @@ function drawTriangle(apex1, apex2, apex3) {
   ctx.fill();
 }
 
+document.addEventListener("DOMContentLoaded", async () => {
+  const spinner = document.querySelector(".spinner");
+  try {
+    const stockSymbols = await fetchStocks();
+    const promises = stockSymbols.map((Symbol) => fetchStockData(Symbol));
+
+    Promise.all(promises).then(() => {
+      spinner.style.display = "none";
+    });
+  } catch (error) {
+    console.error("failed to initialize:", error);
+    spinner.style.display = "none";
+  }
+});
+
+async function fetchStocks() {
+  const response = await fetch("/stocks");
+  if (!response.ok) throw new Error("Failed to fetch stock symbols");
+  const { stockSymbols } = await response.json();
+  return stockSymbols;
+}
+
+async function fetchStockData(symbol) {
+  try {
+    const response = await fetch("/stocks/${symbol}");
+    if (!response.ok) throw new Error("Failed to fetch data for ${symbol}");
+    const data = await response.json();
+    console.log("Data for ${symbol}:", data);
+    //process and display this data as needed
+  } catch (error) {
+    console.error("Error fetching data for ${symbol}:", error);
+  }
+}
+
 drawLine([50, 50], [50, 550]);
 drawTriangle([35, 50], [65, 50], [50, 35]);
 
